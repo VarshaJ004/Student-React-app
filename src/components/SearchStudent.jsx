6df5aafd1148c51ef474819b8ed7edf5390dc112
VarshaJ004 <varshajohnson333@gmail.com>
2026-07-09 23:06:04 +0530

    Second page-SearchStudent created

---
+++ src/components/SearchStudent.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+
+
+const SearchStudent = () => {
+  return (
+    <div>
+     
+    <div className="container mt-5">
+    <div className='page-box '>
+      
+      
+      <h1 className='page-heading mb-4'><center>Search Student</center></h1>
+     
+      <div className="container">
+      <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+        <div className="row g-3">
+          <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+            <label htmlFor="" className="form-label">Admission Number</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <button className="btn btn-primary mt-3">Search</button>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default SearchStudent
